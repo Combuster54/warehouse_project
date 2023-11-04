a7555593735e991abf0c11dcb4d69e9e9bd2ccf9
Ruben <rubenovidalo@gmail.com>
2023-11-04 12:33:07 +0000

    unitary testing

--- nav2_apps/scripts/follow_robot_cart_frame.py
+++ nav2_apps/scripts/follow_robot_cart_frame.py
@@ -8,13 +8,13 @@ import math
 import tf2_py as tf2
 from tf2_ros import LookupException, ConnectivityException, ExtrapolationException
 
-class RobotChaseNode(Node):
+class FollowCartFrame(Node):
 
     def __init__(self):
         super().__init__('robot_chase')
 
         # Crear el publisher
-        qos_profile = QoSProfile(depth=5)
+        qos_profile = QoSProfile(depth=10)
         self.publisher_ = self.create_publisher(Twist, 'robot/cmd_vel', qos_profile)
 
         # Crear el buffer y el listener de transformaciones
@@ -23,20 +23,14 @@ class RobotChaseNode(Node):
 
         # Crear el timer
         self.timer_ = self.create_timer(0.2, self.send_command)
-        self.cancel = False
+
         # Inicializar la velocidad
         self.cmd_vel = Twist()
 
-
     def send_command(self):
-        if(self.cancel):
-            self.timer_.cancel()
-            self.destroy_node()
-            print("Destroy!")
-            
         try:
-
-            transform = self.tf_buffer_.lookup_transform('robot_base_footprint', 'robot_cart_laser', rclpy.time.Time())
+            # Obtener la transformación entre "rick/base_link" y "morty/base_link"
+            transform = self.tf_buffer_.lookup_transform('robot_base_footprint', 'robot_cart_frame', rclpy.time.Time())
 
             # Calcular la distancia y el error angular entre los frames de referencia
             error_distance = math.sqrt(pow(transform.transform.translation.x, 2) +
@@ -52,38 +46,26 @@ class RobotChaseNode(Node):
 
             # Verificar si Rick ya alcanzó a Morty
             if error_distance <= 0.20:
-                self.get_logger().info('cart_frame reached!')
+                self.get_logger().info('Reached!')
                 self.cmd_vel.angular.z = 0
                 self.cmd_vel.linear.x = 0
-                self.cancel = True
-                print(self.cancel)
-
+                self.timer_.cancel()
             else:
                 # Sigue a Morty
                 self.cmd_vel.angular.z = angular_vel
                 self.cmd_vel.linear.x = linear_vel
-
 
             # Publicar la velocidad
             self.publisher_.publish(self.cmd_vel)
 
         except (LookupException, ConnectivityException, ExtrapolationException) as e:
             self.get_logger().warn('TF2 error: {}'.format(str(e)))
-            #self.timer_.cancel()
-           # print("END")
-            print(self.cancel)
-            if(self.cancel):
-                self.timer_.cancel()
-                self.destroy_node()
-                print("Destroy!")
 
 def main(args=None):
     rclpy.init(args=args)
 
-    robot_chase = RobotChaseNode()
-
+    robot_chase = FollowCartFrame()
     rclpy.spin(robot_chase)
-
     robot_chase.destroy_node()
     rclpy.shutdown()
 
--- nav2_apps/scripts/move_shelf_to_ship.py
+++ nav2_apps/scripts/move_shelf_to_ship.py
@@ -12,7 +12,7 @@ from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 from geometry_msgs.msg import Polygon,Point32, Twist
 from std_msgs.msg import Empty
-from math import cos, sin, pi
+from std_msgs.msg import String
 
 #Positions to RB-1 Robot based on map 
 #frame_id: map
@@ -28,11 +28,27 @@ RB1_positions = {
                         0.000, 0.000, -0.689, 0.725]
     }
 
-# Shipping destination for picked products
-shipping_destinations = {
-    "shipping_position":[0.281, -3.0, 0.000,
-                        0.000, 0.000, -0.689, 0.725]
-    }
+class MoveShelfNode(Node):
+
+    def __init__(self):
+        super().__init__('elevator_node')
+        self.publisher_down_ = self.create_publisher(String, '/elevator_down', 10)
+        self.publisher_up_ = self.create_publisher(String, '/elevator_up', 10)
+        self.elevator_msg = String()
+        self.elevator_msg.data = ""   # this will always be blank string!
+        return None
+
+    def publish_message_down(self):
+        self.publisher_down_.publish(self.elevator_msg)
+        self.get_logger().info(f'[Down] Publish: "{self.elevator_msg.data}"')
+        time.sleep(5.0)
+        return None
+
+    def publish_message_up(self):
+        self.publisher_up_.publish(self.elevator_msg)
+        self.get_logger().info(f'[Up] Publish: "{self.elevator_msg.data}"')
+        time.sleep(5.0)
+        return None
 
 rclpy.init()
 
@@ -40,14 +56,7 @@ rclpy.init()
 state_nav= 0
 
 navigator = BasicNavigator()
-
-#create client for service approach
-client_cb_group = MutuallyExclusiveCallbackGroup()
-# cli = navigator.create_client(GoToLoading, '/approach_shelf',callback_group=client_cb_group)
-# while not cli.wait_for_service(timeout_sec=1.0):
-#     navigator.get_logger().info('service not available, waiting again...')
-#create publish
-pub_shelf_down = navigator.create_publisher(Empty,'/elevator_down',10)
+pub_shelf_down = navigator.create_publisher(Empty,'/elevator_down',1)
 pub_local = navigator.create_publisher(Polygon, '/local_costmap/footprint', 10)
 pub_global = navigator.create_publisher(Polygon, '/global_costmap/footprint', 10)
 pub_cmd_vel = navigator.create_publisher(Twist,'robot/cmd_vel',10)
@@ -86,136 +95,32 @@ def publish_footprint_shelf():
     pub_local.publish(footprint)
     pub_global.publish(footprint)
 
-def publish_footprint_robot():
+def pub_initial_footprint():
     # publish footprint
     # Crear los puntos que forman el polígono
     footprint = Polygon()
     points = []
-    for angle in range(0, 360, 10):
-        point = Point32()
-        point.x = 0.25 * cos(angle * pi / 180)  # Radio de 0.25
-        point.y = 0.25 * sin(angle * pi / 180)  # Radio de 0.25
-        points.append(point)
-        # Asignar los puntos al polígono
-        footprint.points = points
-        
-        pub_local.publish(footprint)
-        pub_global.publish(footprint)
 
-def go_to_point2():
+    point1 = Point32()
+    point1.x = 0.25
+    point1.y = 0.25
 
-    print('Got product from ' + request_item_location +
-              '! Bringing product to shipping destination (' + request_destination + ')...')
-        
-    shipping_destination = PoseStamped()
-    shipping_destination.header.frame_id = 'map'
-    shipping_destination.header.stamp = navigator.get_clock().now().to_msg()
-    shipping_destination.pose.position.x = shipping_destinations[request_destination][0]
-    shipping_destination.pose.position.y = shipping_destinations[request_destination][1]
-    shipping_destination.pose.position.z = shipping_destinations[request_destination][2]
-    shipping_destination.pose.orientation.x = shipping_destinations[request_destination][3]
-    shipping_destination.pose.orientation.y = shipping_destinations[request_destination][4]
-    shipping_destination.pose.orientation.z = shipping_destinations[request_destination][5]
-    shipping_destination.pose.orientation.w = shipping_destinations[request_destination][6]
-    navigator.goToPose(shipping_destination)
-    i = 0
-    while not navigator.isTaskComplete():
-        i = i + 1
-        feedback = navigator.getFeedback()
-        if feedback and i % 5 == 0:
-            print('Estimated time of arrival at ' + request_item_location +
-                ' for worker: ' + '{0:.0f}'.format(
-                Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
-                + ' seconds.')
+    point2 = Point32()
+    point2.x = 0.25
+    point2.y = -0.25
 
-def down_shelf():
-    print('Down shelf')
-    msg_pub = Empty()
-    pub_shelf_down.publish(msg_pub)
-    
-    duration = Duration(seconds=5)
-    rate = navigator.create_rate(10, navigator.get_clock())
-    start_time = navigator.get_clock().now()
-    while rclpy.ok() and (navigator.get_clock().now() - start_time) < duration:
-        rate.sleep
-def exit_under_the_shelf():
-    # publish for fordward
-    msg_vel = Twist()
-    msg_vel.linear.x = 0.1
-    duration = Duration(seconds=7)
-    rate = navigator.create_rate(10, navigator.get_clock())
-    start_time = navigator.get_clock().now()
-    while rclpy.ok() and (navigator.get_clock().now() - start_time) < duration:
-        msg = Twist()
-        msg.linear.x = 0.1
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = 0.0
-        pub_cmd_vel.publish(msg)
-        print('moving forward')
-        rate.sleep
-    print('stop')
-    msg.linear.x = 0.0
-    pub_cmd_vel.publish(msg)
+    point3 = Point32()
+    point3.x = -0.25
+    point3.y = -0.25
 
-def exit_with_shelf():
-    # publish for backward
-    msg_vel = Twist()
-    msg_vel.linear.x = -0.1
-    #duration = Duration(seconds=12)
-    duration = Duration(seconds=26)
-    rate = navigator.create_rate(10, navigator.get_clock())
-    start_time = navigator.get_clock().now()
-    while rclpy.ok() and (navigator.get_clock().now() - start_time) < duration:
-        msg = Twist()
-        msg.linear.x = -0.1
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = 0.0
-        pub_cmd_vel.publish(msg)
-        print('moving backward with shelf')
-        rate.sleep
-    print('stop')
-    msg.linear.x = 0.0
-    pub_cmd_vel.publish(msg)
-    #turn
-    duration = Duration(seconds=3,nanoseconds=500000000)
-    start_time = navigator.get_clock().now()
-    while rclpy.ok() and (navigator.get_clock().now() - start_time) < duration:
-        msg = Twist()
-        msg.linear.x = 0.05
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = -0.30
-        pub_cmd_vel.publish(msg)
-        print('turn with shelf')
-        rate.sleep
-    print('stop')
-    msg.linear.x = 0.1
-    pub_cmd_vel.publish(msg)
-    #forward
-    duration = Duration(seconds=3)
-    start_time = navigator.get_clock().now()
-    while rclpy.ok() and (navigator.get_clock().now() - start_time) < duration:
-        msg = Twist()
-        msg.linear.x = 0.05
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = -0.30
-        pub_cmd_vel.publish(msg)
-        print('moving backward with shelf')
-        rate.sleep
-    print('stop')
-    msg.linear.x = 0.0
-    pub_cmd_vel.publish(msg)
+    point4 = Point32()
+    point4.x = -0.25
+    point4.y =  0.25
+
+    footprint.points = [point1, point2, point3, point4]
+
+    pub_local.publish(footprint)
+    pub_global.publish(footprint)
 
 def set_pos_init():
 
@@ -236,21 +141,6 @@ def set_pos_init():
     # Wait for navigation to activate fully
     navigator.waitUntilNav2Active()
 
-def call_service():
-    global state_nav
-    global cli
-    # once arrived at loading position step to execute the approach service
-    navigator.req = GoToLoading.Request()
-    navigator.req.attach_to_shelf = True
-    navigator.future = cli.call_async(navigator.req)
-    rclpy.spin_until_future_complete(navigator, navigator.future)
-    status = navigator.future.result()
-    if status != True:
-        #navigator.error('failed service')
-        state_nav=3
-    else:
-        navigator.info('successful service!')
-        state_nav=3
 
 def go_pose(go_pose_dic, key_dic, state_num):
     global state_nav
@@ -297,9 +187,9 @@ def go_pose(go_pose_dic, key_dic, state_num):
     while not navigator.isTaskComplete():
         pass
 
-def self_localize():
-    #rotate 2 sec for autolocalization
-    pass
+# def self_localize():
+#     #rotate 2 sec for autolocalization
+#     pass
 
 def main():
     global state_nav
@@ -309,31 +199,27 @@ def main():
         if state_nav==1:
             print(state_nav)
             #self_localize()
-            #set_pos_init()
+            set_pos_init()
             go_pose(RB1_positions,'loading_position',0)
             print(RB1_positions['loading_position'])
             exit(0)
-        # if state_nav==2:
-        #     print(state_nav)
-        #     call_service()
-        # if state_nav==3:
-        #     print(state_nav)
-        #     publish_footprint_shelf()
-        #     exit_with_shelf()
-        #     go_pose(shipping_destinations,request_destination,4)
-        # if state_nav==4:
-        #     print(state_nav)
-        #     down_shelf()
-        #     exit_under_the_shelf()
-        #     publish_footprint_robot()
-        #     go_pose(RB1_positions,request_init_position,0)
-        #     exit(0)
-        # if state_nav==1:
-        #     exit(0)
-
-
+        if state_nav==2:
+            print(state_nav)
+            #call follow robot_cart_frame  && state_nav=3
+        if state_nav==3:
+            print(state_nav)
+            publish_footprint_shelf()
+            #Do some movements with navigator.backup nad navigator.rotate
+            go_pose(RB1_positions,'shipping_position',4)
+        if state_nav==4:
+            print(state_nav)
+            #down Shelf && backup && spin 180 degrees
+            pub_initial_footprint()
+            go_pose(RB1_positions,'init_position',0)
+            exit(0)
+        if state_nav==1:
+            exit(0)
     exit(0)
-
 
 if __name__ == '__main__':
     main()
